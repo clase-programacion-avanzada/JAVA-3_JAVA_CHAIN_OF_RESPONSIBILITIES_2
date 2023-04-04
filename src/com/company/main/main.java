be0ae3fde0b22fc3b03ef31f5076ef4d3337a317
Diego Fernando Mora Velasco <difemove@gmail.com>
2023-04-04 09:01:36 -0500

    Added project to git

---
+++ src/com/company/main/main.java
@@ -0,0 +1,15 @@
+package com.company.main;
+
+import com.company.model.Animal;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Animal dog = new Animal("Paco", 3);
+		
+		dog.setName("Fabricio");
+		
+		System.out.println(dog.getName());
+	}
+}
---
+++ src/com/company/model/Animal.java
@@ -0,0 +1,39 @@
+package com.company.model;
+
+public class Animal {
+
+	private String name;
+	private int age;
+	
+	public Animal(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
