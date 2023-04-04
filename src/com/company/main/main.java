5b102b7b89865da7e7d6108666da8b55cca81684
Diego Fernando Mora Velasco <difemove@gmail.com>
2023-04-04 09:11:04 -0500

    Added animals list to Main

--- src/com/company/main/main.java
+++ src/com/company/main/main.java
@@ -1,12 +1,23 @@
 package com.company.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.company.model.Animal;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		List<Animal> animals = new ArrayList<Animal>();
+		
 		Animal dog = new Animal("Paco", 3);
+		
+		animals.add(dog);
+		
+		Animal cat = new Animal("Amelia", 4);
+		
+		animals.add(cat);
 		
 		dog.setName("Fabricio");
 		
